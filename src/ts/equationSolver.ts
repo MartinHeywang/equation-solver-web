a75665da385e76e922d1ba3d65e7a889e0a32454
Martin Heywang <martin.heywang1@gmail.com>
2022-01-18 12:59:40 +0100

    Fixed problem with trinomial solver

--- src/ts/equationSolver.ts
+++ src/ts/equationSolver.ts
@@ -21,7 +21,7 @@ const solvers = {
         if (a === undefined) return [];
         if (a === 0) return b === y ? [Infinity] : [];
 
-        const delta = Math.pow(-(b || 0), 2) - 4 * a * (c || 0);
+        const delta = Math.pow(-(b || 0), 2) - 4 * a * ((c || 0) - (y || 0));
 
         if (delta > 0) {
             const x1 = (-(b || 0) - Math.sqrt(delta)) / (2 * a);
